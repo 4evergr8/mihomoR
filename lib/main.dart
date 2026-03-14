f3df6051b8de7e9b2b1283d509c53fefc5dbd1fd
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-03-15 01:01:00 +0800

    Revert "Update main.dart"
    This reverts commit c2a362a1336843f20de8a89590c1aef71c21e21d.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,13 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:quick_settings/quick_settings.dart';
 import '../theme/theme.dart';
 import '../theme/util.dart';
 import '../widget.dart';
-import '../service/yaml.dart';
 
-const String settingsPath = '/data/adb/mihomo/settings.yaml';
+
+
+
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  QuickSettings.listen((event) async {
-    try {
-      final settings = await readYamlAsObject(settingsPath);
-
-      final stopCmd = settings['kill'] ?? '';
-      final startCmd = settings['start'] ?? '';
-
-      await Process.run("sh", ["-c", stopCmd]);
-      await Process.start("sh", ["-c", startCmd]);
-
-    } catch (_) {}
-  });
-
   runApp(const MyApp());
 }
 
@@ -34,13 +18,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final brightness = View.of(context).platformDispatcher.platformBrightness;
 
+    // 创建自定义的主题
     TextTheme textTheme = createTextTheme(context, "Noto Sans", "Noto Sans");
     MaterialTheme theme = MaterialTheme(textTheme);
 
     return MaterialApp(
       title: 'mihomoR',
       theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-      home: const HomeScreen(),
+      home: const HomeScreen(), // 使用 HomeScreen 组件
     );
   }
 }
@@ -57,14 +42,12 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
 
-    QuickSettings.addTileToQuickSettings(
-      label: "重启mihomo",
-      drawableName: "quick_settings_base_icon",
-    );
   }
+
+
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavBar();
+    return BottomNavBar(); // 仅显示底部导航栏
   }
 }
