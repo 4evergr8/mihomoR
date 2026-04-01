90bd7a43a92c99df2b67e70b387b30f609808d81
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-04-01 15:58:35 +0800

    Update override.dart

--- mihomo/override.dart
+++ mihomo/override.dart
@@ -7,43 +7,6 @@ dynamic override(dynamic input) {
       "auto-route": true,
       "auto-detect-interface": true,
       "strict-route": true,
-      "exclude-package": [
-        "com.byyoung.setting",
-        "com.tencent.mm",
-        "com.xunmeng.pinduoduo",
-        "com.zhihu.android",
-        "com.autonavi.minimap",
-        "com.taobao.taobao",
-        "com.android.bluetooth",
-        "com.eg.android.AlipayGphone",
-        "org.localsend.localsend_app",
-        "com.coolapk.market",
-        "com.coloros.accessibilityassistant",
-        "com.oplus.blacklistapp",
-        "com.coloros.directui",
-        "com.coloros.ocrscanner",
-        "com.quark.browser",
-        "tv.danmaku.bili",
-        "com.xunlei.downloadprovider",
-        "com.tmall.wireless",
-        "com.oplus.cast",
-        "com.ss.android.ugc.aweme",
-        "com.bilibili.app.in",
-        "com.heytap.cloud",
-        "com.mfcloudcalculate.networkdisk",
-        "com.tencent.androidqqmail",
-        "com.oplus.account",
-        "com.hpbr.bosszhipin",
-        "com.MobileTicket",
-        "com.baidu.netdisk",
-        "com.bilibili.studio",
-        "com.tencent.tim",
-        "com.coloros.translate",
-        "com.taobao.litetao",
-        "com.oplus.vdc",
-        "com.heytap.speechassist",
-        "com.oplus.aimemory"
-      ]
     },
     "dns": {
       "enable": true,
@@ -73,8 +36,6 @@ dynamic override(dynamic input) {
 
   // 输出对象，拷贝输入
   final dynamic out = Map.from(input);
-
-  // 替换 dns 和 tun
   out["dns"] = template["dns"];
   out["tun"] = template["tun"];
 
