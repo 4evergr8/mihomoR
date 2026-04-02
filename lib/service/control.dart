2b2da18129224c4f15c3ebd091dd0595ce5767d4
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-04-02 21:54:57 +0800

    Update control.dart

--- lib/service/control.dart
+++ lib/service/control.dart
@@ -4,31 +4,14 @@ import 'package:mihomoR/service/subscriptions.dart';
 import 'package:quick_settings_with_flutter_plugins/quick_settings.dart';
 
 Future<void> stopMihomo() async {
-  await QuickSettings.syncTile(
-    Tile(
-      label: "mihomo",
-      tileStatus: TileStatus.inactive,
-      drawableName: 'quick_settings_base_icon',
-      contentDescription: "mihomo 已停止",
-    ),
-  );
   final settings = await readYamlAsMap(settingsPath);
   final stopCmd = settings['kill'] ?? '';
   if (stopCmd.isNotEmpty) {
     await Process.run("sh", ["-c", stopCmd]);
   }
-
 }
 
 Future<void> startMihomo() async {
-  await QuickSettings.syncTile(
-    Tile(
-      label: "mihomo",
-      tileStatus: TileStatus.active,
-      drawableName: 'quick_settings_base_icon',
-      contentDescription: "mihomo 已启动",
-    ),
-  );
   final settings = await readYamlAsMap(settingsPath);
   final stopCmd = settings['kill'] ?? '';
   final startCmd = settings['start'] ?? '';
