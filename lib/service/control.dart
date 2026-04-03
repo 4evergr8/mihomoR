3618f597c703fd35d3bb59e2ccef3cb2c406fde6
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-04-04 03:19:44 +0800

    Update control.dart

--- lib/service/control.dart
+++ lib/service/control.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:mihomoR/service/path.dart';
 import 'package:mihomoR/service/subscriptions.dart';
-import 'package:quick_settings_with_flutter_plugins/quick_settings.dart';
 
 Future<void> stopMihomo() async {
   final settings = await readYamlAsMap(settingsPath);
@@ -16,12 +15,9 @@ Future<void> startMihomo() async {
   final stopCmd = settings['kill'] ?? '';
   final startCmd = settings['start'] ?? '';
 
-  // 先停止
   if (stopCmd.isNotEmpty) {
     await Process.run("sh", ["-c", stopCmd]);
   }
-
-  // 再启动
   if (startCmd.isNotEmpty) {
     await Process.start("sh", ["-c", startCmd]);
   }
