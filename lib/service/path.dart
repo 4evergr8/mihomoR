818d5065301f1504e2f5118cd114f76ccefb865d
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-07-04 19:14:10 +0800

    Update path.dart

--- lib/service/path.dart
+++ lib/service/path.dart
@@ -2,4 +2,4 @@ final String mainPath = '/data/adb/modules/ClashRoot';
 final String dataPath = '$mainPath/data.yaml';
 final String configPath = '$mainPath/config.yaml';
 final String overridePath = '$mainPath/override.yaml';
-final String scriptPath = '$mainPath/boot-completed.sh';
+final String scriptPath = '$mainPath/clash.sh';
