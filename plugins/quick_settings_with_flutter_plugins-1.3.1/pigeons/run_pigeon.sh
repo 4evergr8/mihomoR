7e0e5d0a8b3dd8d6620431753c8e3da7db4c730e
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-07-05 15:08:22 +0800

    a

--- plugins/quick_settings_with_flutter_plugins-1.3.1/pigeons/run_pigeon.sh
+++
@@ -1,5 +0,0 @@
-flutter pub run pigeon \
-  --input pigeons/quick_settings_interface.dart \
-  --dart_out lib/src/quick_settings_interface.dart \
-  --experimental_kotlin_out ./android/src/main/kotlin/io/apparence/quick_settings/pigeon/QuickSettingsInterface.kt \
-  --experimental_kotlin_package "io.apparence.quick_settings.pigeon"
